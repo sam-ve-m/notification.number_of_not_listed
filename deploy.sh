187be58691a63cbc8190e4c39df98e843904177f
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 17:43:08 -0300

    feat: versioning fissions deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
 fission spec init
-fission env create --spec --name notification-count-unlisted-env --image nexus.sigame.com.br/fission-async:0.1.9 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
-fission fn create --spec --name notification-count-unlisted-fn --env notification-count-unlisted-env --src "./func/*" --entrypoint main.get_number_of_unlisted_notifications --executortype newdeploy --maxscale 1
-fission route create --spec --name notification-count-unlisted-rt --method GET --url /notifications/count_unlisted --function notification-count-unlisted-fn
+fission env create --spec --name notification-count-miss-env --image nexus.sigame.com.br/fission-notification-count-miss:0.1.0-2 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name notification-count-miss-fn --env notification-count-miss-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission route create --spec --name notification-count-miss-rt --method GET --url /notifications/count_unlisted --function notification-count-miss-fn
